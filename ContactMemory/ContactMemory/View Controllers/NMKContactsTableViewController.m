821e564feaa60f0dd006a058014e6f3e425aa4d8
Nar Kumar <njn.kumar@gmail.com>
2020-01-29 17:51:03 -0800

    TVC Completed

--- ContactMemory/ContactMemory/View Controllers/NMKContactsTableViewController.m
+++ ContactMemory/ContactMemory/View Controllers/NMKContactsTableViewController.m
@@ -7,6 +7,9 @@
 //
 
 #import "NMKContactsTableViewController.h"
+#import "NMKContact.h"
+#import "NMKContactController.h"
+#import "NMKDetailViewController.h"
 
 @interface NMKContactsTableViewController ()
 
@@ -14,15 +17,31 @@
 
 @implementation NMKContactsTableViewController
 
+- (instancetype)initWithCoder:(NSCoder *)coder {
+    self = [super initWithCoder:coder];
+    if (self) {
+        _controller = [[[[NMKContactController alloc] init] retain] autorelease];
+    }
+    return self;
+}
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    [self testContacts];
+    [self.tableView reloadData];
 }
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    [self.tableView reloadData];
+}
+
+- (void)testContacts {
+    [self.controller createContactWithName:@"Nar Kumar" phone:@"(714) 390-9928" email:@"njn.kumar@gmail.com"];
+    [self.controller createContactWithName:@"Nidhi Sheth" phone:@"(909) 267-8328" email:@"nidhishethp@gmail.com"];
+}
+
 
 #pragma mark - Table view data source
 
@@ -31,62 +50,34 @@
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-#warning Incomplete implementation, return the number of rows
-    return 0;
+    return self.controller.contacts.count;
 }
 
-/*
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    UITableViewCell *cell = [[[tableView dequeueReusableCellWithIdentifier:@"ContactCell" forIndexPath:indexPath] retain] autorelease];
     
-    // Configure the cell...
+    NMKContact *contact = [[self.controller.contacts[indexPath.row] retain] autorelease];
+    
+    cell.textLabel.text = contact.name;
+    cell.detailTextLabel.text = contact.phone;
     
     return cell;
 }
-*/
 
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+    if ([segue.identifier isEqualToString:@"DetailSegue"]) {
+        NMKDetailViewController *destinationVC = (NMKDetailViewController *)[segue destinationViewController];
+        NSIndexPath *indexPath = [self.tableView indexPathForSelectedRow];
+        NMKContact *contact = self.controller.contacts[indexPath.row];
+        destinationVC.contact = contact;
+        destinationVC.controller = self.controller;
+    } else if ([segue.identifier isEqualToString:@"AddSegue"]) {
+        NMKDetailViewController *destinationVC = (NMKDetailViewController *)[segue destinationViewController];
+        destinationVC.controller = self.controller;
+    }
 }
-*/
 
 @end
